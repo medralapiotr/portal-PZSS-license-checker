8f513aa368b5623a415427daca857787add9febe
Piotr.Medrala <piotr.medrala1@gmail.com>
2021-11-30 19:55:24 +0100

    Updated Readme file, General code cleanup, replacing multiple website get calls and email sending with functions

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Script to be used to check and upadte via email status of license on PZSS portal.
 PZSS portal website: https://portal.pzss.org.pl/
+
+Caveat:
+Gmail currently blocks login from 3rd parties
+Solution will not work with emails with 2FA
+Above could be resolved by creating dummy email account on gmail only for this purpose and switching on Less secure app access (Link below)
+https://support.google.com/accounts/answer/6010255?hl=en#zippy=%2Cuse-an-app-password
--- pzss-license-checker.py
+++ pzss-license-checker.py
@@ -1,6 +1,6 @@
-## Script to validate PZSS license status
-## Check is done in 5 min intervals
-## When License will be avaiable there will be email notification
+# Script to validate PZSS license status
+# Check is done in 5 min intervals
+# When License will be avaiable there will be email notification
 
 import time
 from datetime import datetime
@@ -9,95 +9,95 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-## User input to pass credentials and logins
+# User input to pass credentials and logins
 
 login = input("Please enter login to PZSS portal: ")
 password = input("Please enter password to PZSS portal: ")
 email = input("Please enter your email adress: ")
 email_pass = input("Please enter your email password: ")
-strona = 'https://portal.pzss.org.pl'
-strona_2 = "https://portal.pzss.org.pl/Player/Applications"
+website = 'https://portal.pzss.org.pl'
+website_2 = "https://portal.pzss.org.pl/Player/Applications"
 
-## Creating session on PZSS website and checking initial status of the license and send the email with current status of the license
-s = requests.session()
 
-r = s.post(url=strona, json={"Login": login, "Password": password})
+# Static variables to be used within functions
+initial_subject = "Initial PZSS license status"
+update_subject = "New PZSS license status"
 
-r2 = s.get(url=strona_2)
+# Function to retrieve license status from website
 
-p1 = BeautifulSoup(r2.text, 'lxml').prettify()
-p2 = pd.read_html(p1, header=0, index_col=None)[0]
-result_initial = p2.iat[0, 3]
+
+def license_status():
+    s = requests.session()
+
+    r = s.post(url=website, json={"Login": login, "Password": password})
+
+    r2 = s.get(url=website_2)
+
+    p1 = BeautifulSoup(r2.text, 'lxml').prettify()
+    p2 = pd.read_html(p1, header=0, index_col=None)[0]
+    return p2.iat[0, 3]
+
+
+# Function to send email with any update to the license status
+def send_email(contents, email_subject):
+    yag = yagmail.SMTP(email, email_pass)
+    yag.send(
+        to=email,
+        subject=email_subject,
+        contents=contents,
+    )
+
+
+# Creating session on PZSS website and checking initial status of the license
+# send the email with current status of the license
+# Print initial license status in console with timestamp
+
+result_initial = license_status()
 result = result_initial
-
-yag = yagmail.SMTP(email, email_pass)
+mail_subject = initial_subject
 content = str(datetime.now())+": "+result
-yag.send(
-    to=email,
-    subject="Initial PZSS license status",
-    contents=content,
-)
+send_email(content, mail_subject)
 print(str(datetime.now())+" Initial PZSS license status: "+result)
 
 
-## continuously loop with request to the portal in search for a change of license status in 5 min intervals
+# continuously loop with request to the portal in search for a change of license status in 5 min intervals
 
 while result_initial == result:
-    s = requests.session()
-    r = s.post(url=strona,json={"Login": login,"Password": password})
-    r2 = s.get(url= strona_2)
+    result = license_status()
 
-    p1 = BeautifulSoup(r2.text, 'lxml').prettify()
-    p2 = pd.read_html(p1, header = 0, index_col= None ) [0]
-    result = p2.iat[0,3]
-
-## Penultimate status is "Zatwierdzony przez WZSS  oczekiwanie na decyzję PZSS"
-## when reaching penultimate status send email update, exit loop and enter final loop in search of final status change
+# Penultimate status is "Zatwierdzony przez WZSS  oczekiwanie na decyzję PZSS"
+# when reaching penultimate status
+# send email update,print status into console, exit loop and enter final loop in search of final status change
     if result == "Zatwierdzony przez WZSS  oczekiwanie na decyzję PZSS":
         content = str(datetime.now()) + ": " + result
-        yag.send(
-            to=email,
-            subject="New PZSS license status",
-            contents = content,
-                    )
+        mail_subject = update_subject
+        send_email(content, mail_subject)
         print(str(datetime.now()) + " Current license status: " + result)
         time.sleep(300)
         result_initial = result
         break
 
-## should there be a change with the status, send the email with new status and search for further status change
+# should there be a change with the status, send the email with new status and search for further status change
     if result_initial != result:
         result_initial = result
         content = str(datetime.now()) + ": " + result
-        yag.send(
-            to=email,
-            subject="New PZSS license status",
-            contents = content,
-                    )
+        mail_subject = update_subject
+        send_email(content, mail_subject)
+        print(str(datetime.now()) + " Current license status: " + result)
+        time.sleep(300)
         continue
 
-    print(str(datetime.now())+" Current license status: "+result)
-    time.sleep(300)
 
-##Final loop to break when final status change will happen
+# Final loop to break when final status change will happen
 while result_initial == result:
-    r = s.post(url=strona,json={"Login": login,"Password": password})
-    r2 = s.get(url= strona_2)
-
-    p1 = BeautifulSoup(r2.text, 'lxml').prettify()
-    p2 = pd.read_html(p1, header = 0, index_col= None ) [0]
-    result = p2.iat[0,3]
-    if result !="Zatwierdzony przez WZSS  oczekiwanie na decyzję PZSS":
+    result = license_status()
+    if result != "Zatwierdzony przez WZSS  oczekiwanie na decyzję PZSS":
         break
     print(str(datetime.now())+" Current license status: "+result)
     time.sleep(300)
 
-#After final status change on website code to send final email update and exit
+# After final status change on website code to send final email update and exit
 content = str(datetime.now())+": "+result
-yag.send(
-    to=email,
-    subject="New PZSS license status",
-    contents=content,
-              )
-
+mail_subject = update_subject
+send_email(content, mail_subject)
 print(str(datetime.now()) + " Current license status: " + result)
